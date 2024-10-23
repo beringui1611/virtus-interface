c7bf94fa77e06176e61c0f46286b6c60b7a638c1
Next Chain <166194690+nxtchain@users.noreply.github.com>
2024-10-23 20:50:06 -0300

    update.1

--- virtus-interface/src/App.tsx
+++ virtus-interface/src/App.tsx
@@ -1,7 +1,9 @@
 // import ConnectButton from './components/ConnectButton';
 // import { useReadContract } from 'wagmi'
 // import USDTAbi  from './abiUSDT.json';
-
+import CardPeople from "./assets/cardstudents.png";
+import Square from "./assets/square.png";
+import Student from "./assets/student.png";
 function App() {
 
   // const USDTAddress = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F';
@@ -16,14 +18,37 @@ function App() {
   return (
   
       <section className='bg-[#102A2B] h-screen'>
-        {/* <ConnectButton />
+         <div className="absolute w-20 h-20 bg-white blur-xl bg-[#138471]"></div>
+         <div className="flex justify-between items-center p-5">
+            <div className="flex flex-col gap-2">
+              <img className="max-w-32 z-10" src={CardPeople} alt="students-photo-card"/>
+              <label className="font-primary text-white z-10">+500 Students</label>
+            </div>
+            
+            <button 
+            className="
+            border-2 rounded-tr-xl rounded-sm 
+            text-white p-1 h-10 border-[#38F682]
+            shadow-md shadow-[#38f682] font-bold">
+              Join Community
+            </button>
+         </div>
 
-        <h1>
-          TOTAL SUPPLY: 
-          {isLoading ? "Loading..." : error ? `Error: ${error.message}` : data ? `${data}` : "No data"}
-        </h1> */}
-
-        <h1 className="text-red-400">TESTE</h1>
+         <article className="mt-20">
+            <div className="flex flex-col">
+              <h1 className="font-primary text-white text-center w-[250px]">
+                GET YOUR PASSPORT 
+                RIGHT NOW<p></p>
+                & LEARN ENGLISH
+              </h1>
+              <button className="bg-[#E748D8] rounded-tr-xl rounded-sm ml-5 mt-5 p-1 rounded text-white font-bold w-32">Buy Now</button>
+            </div>
+            <div className="absolute right-0 w-20 h-20 bg-white blur-xl bg-[#138471]"></div>
+              <div>
+                 <img className="z-20 absolute bottom-0 left-20" src={Student} alt="student-virtus"/>
+                 <img className="absolute z-1 bottom-0" src={Square} alt="buy-nft"/>
+              </div>
+         </article>
       </section> 
   )
 }
