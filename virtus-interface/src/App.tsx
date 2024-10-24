6c416f286fc4f453be03f9cf25228065cfd63206
beringui1611 <fortiniteberingui12@gmail.com>
2024-10-24 14:01:07 -0300

    new

--- virtus-interface/src/App.tsx
+++ virtus-interface/src/App.tsx
@@ -114,6 +114,7 @@ function App() {
               <p className="font-primary text-white flex items-center gap-2">
                 50 USDT 
                 <img className="w-7" src={Usdt} alt="usdt-coin" />
+                <a href=""></a>
               </p>
             </div>
             {   isConnected ?
