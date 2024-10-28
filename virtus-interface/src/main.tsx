e765303969cae7dbab0d167ad730ebc286568be0
beringui1611 <fortiniteberingui12@gmail.com>
2024-10-28 17:33:45 -0300

    new

--- virtus-interface/src/main.tsx
+++ virtus-interface/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { createAppKit } from '@reown/appkit/react';
 import { WagmiProvider } from 'wagmi';
-import { mainnet, polygon } from '@reown/appkit/networks';
+import { polygonAmoy } from '@reown/appkit/networks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
 import "./input.css";
@@ -18,7 +18,7 @@ const queryClient = new QueryClient();
     icons: [ " "]
   }
 
-  const networks = [mainnet, polygon];
+  const networks = [polygonAmoy];
 
   const wagmiAdapter = new WagmiAdapter({
     networks,
@@ -29,7 +29,7 @@ const queryClient = new QueryClient();
 
   createAppKit({
     adapters: [wagmiAdapter],
-    networks: [polygon],
+    networks: [polygonAmoy],
     projectId,
     metadata,
     features: {
