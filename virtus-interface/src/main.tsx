1d5caa305b2d2b905ac1c4a172f63d8f38b2a2f0
beringui1611 <fortiniteberingui12@gmail.com>
2024-12-09 16:48:26 -0300

    new

--- virtus-interface/src/main.tsx
+++ virtus-interface/src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { createAppKit } from '@reown/appkit/react';
-import { WagmiProvider } from 'wagmi';
+import { CreateConnectorFn, http, WagmiProvider } from 'wagmi';
 import { polygon } from '@reown/appkit/networks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
 import "./input.css";
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors';
 
 const queryClient = new QueryClient();
 const projectId = `${import.meta.env.VITE_API_KEY}`;
@@ -16,12 +17,23 @@ const metadata = {
   description: 'Virtush your new method to learn english!',
   url: 'https://payment.virtuscoin.org/',
   icons: [''], 
-  redirect: "https://payment.virtuscoin.org/"
 };
+
+const connectors: CreateConnectorFn[] = [];
+connectors.push(walletConnect({projectId, metadata, showQrModal: true}));
+connectors.push(injected({shimDisconnect: true}))
+connectors.push(coinbaseWallet({
+  appName: metadata.name,
+  appLogoUrl: metadata.icons[0]
+}))
 
 const networks = [polygon];
 
 const wagmiAdapter = new WagmiAdapter({
+  transports: {
+    [polygon.id]: http()
+  },
+  connectors,
   networks,
   projectId: projectId,
   
@@ -48,4 +60,4 @@ createRoot(document.getElementById('root')!).render(
       </QueryClientProvider>
     </WagmiProvider>
   </StrictMode>
-);
+); 
