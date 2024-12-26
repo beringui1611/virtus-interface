5f13cd3246077771aa58d528943c34af38bc46c5
beringui1611 <fortiniteberingui12@gmail.com>
2024-12-26 14:06:38 -0300

    v.0.2

--- virtus-interface/i18n/i18n.ts
+++ virtus-interface/i18n/i18n.ts
@@ -1,9 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector'; // Importando o detector de idioma
+
+
 import translationEn from "./lang/en-us.json";
 import translationBr from './lang/pt-br.json';
+import translationEs from './lang/es.json';
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
@@ -12,13 +17,19 @@ i18n
       },
       pt: {
         translation: translationBr
+      },
+      es:{
+        translation: translationEs
       }
     },
-    lng: navigator.language.split("-")[0] || "en",
     fallbackLng: "en",
     interpolation: {
-      escapeValue: false, // React já faz escaping
+      escapeValue: false,
     },
+    detection: {
+        order: ['querystring', 'cookie', 'localStorage', 'navigator'],
+        caches: ['cookie'],
+    }
   });
 
 export default i18n;
